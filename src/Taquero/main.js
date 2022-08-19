28a6e254b76805d72c6ae18dc8bbec94ebcbc5b9
Mauricio-Rubio <mau.r.h54@gmail.com>
2022-08-18 21:41:27 -0500

    Configuration

---
+++ src/Taquero/main.js
@@ -0,0 +1,8 @@
+console.log('Taquero');
+console.log('Taco 1');
+console.log('Taco 2');
+console.log('Taco 3');
+setTimeout((function() { 
+    console.log('Torta');
+}), 1000)
+console.log('Taco 4');
